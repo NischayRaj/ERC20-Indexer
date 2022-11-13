82b40df6f706c78bffc9f1aa114b2961d475f855
NischayRaj <112957967+NischayRaj@users.noreply.github.com>
2022-11-13 19:03:05 +0530

    Create package.java

---
+++ package.java
@@ -0,0 +1,17 @@
+
+package erc20.indexer;
+
+/**
+ *
+ * @author nisch
+ */
+public class ERC20Indexer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
